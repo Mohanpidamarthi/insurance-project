177ca9446502b6fdfbcc32edf28241e8d510b160
Mohanpidamarthi <102550460+Mohanpidamarthi@users.noreply.github.com>
2022-03-29 17:33:58 +0530

    Delete Dvara directory

--- Dvara/DvaraProj/Taskapp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Category, SubCategory
-
-
-class UploadFileForm(forms.Form):
-    file = forms.FileField()
--- Dvara/DvaraProj/Taskapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Home, sub_category_list
-
-urlpatterns = [
-    path('', Home.as_view(), name="home"),
-    path('ajax/sub_category_list/', sub_category_list,
-         name='sub_category_list'),
-]
